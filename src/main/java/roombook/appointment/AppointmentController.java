5d2d95aadc5d6ec0d63f02cfc2a516c0c86f819e
Randy Gingeleski <randy.gingeleski@aspectsecurity.com>
2018-03-04 17:57:29 -0500

    close #2

--- src/main/java/roombook/appointment/AppointmentController.java
+++ src/main/java/roombook/appointment/AppointmentController.java
@@ -1,9 +1,14 @@
 package roombook.appointment;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -12,7 +17,44 @@ public class AppointmentController {
 
     private List<Appointment> appointments;
 
-    public AppointmentController() {
+    private Date getDateFromUnixTime(long unixTimestamp) {
+        return new Date(unixTimestamp * 1000L);
+    }
+
+    private void initialLoadFromJsonFile() {
         this.appointments = new ArrayList<>();
+
+        try {
+            String jsonFilePath = AppointmentController.class.getClassLoader().getResource("users.json").getPath();
+
+            JSONParser parser = new JSONParser();
+            JSONArray a = (JSONArray) parser.parse(new FileReader(jsonFilePath));
+
+            for (Object o : a)
+            {
+                Appointment appointment = new Appointment();
+                JSONObject jsonAppointment = (JSONObject) o;
+
+                appointment.setName((String) jsonAppointment.get("name"));
+                appointment.setRoom((String) jsonAppointment.get("room"));
+                appointment.setOwner((String) jsonAppointment.get("owner"));
+                appointment.setCreator((String) jsonAppointment.get("creator"));
+
+                long startTime = (long) jsonAppointment.get("start");
+                appointment.setStart(getDateFromUnixTime(startTime));
+
+                long endTime = (long) jsonAppointment.get("end");
+                appointment.setEnd(getDateFromUnixTime(endTime));
+
+                this.appointments.add(appointment);
+            }
+        }
+        catch (Exception e) {
+            return;
+        }
+    }
+
+    public AppointmentController() {
+        initialLoadFromJsonFile();
     }
 }
--- src/main/java/roombook/user/User.java
+++ src/main/java/roombook/user/User.java
@@ -2,33 +2,22 @@ package roombook.user;
 
 public abstract class User
 {
-    private String name;
     private String username;
     private String password;
 
     public User()
     {
-        this.setName(null);
         this.setUsername(null);
         this.setPassword(null);
     }
 
     public User(String name, String username, String password)
     {
-        this.setName(name);
         this.setUsername(username);
         this.setPassword(password);
     }
 
     public abstract String getType();
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public String getUsername() {
         return username;
