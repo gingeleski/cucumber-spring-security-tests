681f23a0cffe2bf4bbe2a880a95825a6319fede4
Randy Gingeleski <randy.gingeleski@aspectsecurity.com>
2018-03-06 13:19:57 -0500

    close #17

--- src/test/java/cucumber/security/RequestStepDefs.java
+++ src/test/java/cucumber/security/RequestStepDefs.java
@@ -3,8 +3,15 @@ package cucumber.security;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
 import cucumber.api.java.en.Then;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-public class RequestStepDefs {
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class RequestStepDefs extends IntegrationTestingBase {
+
+    private ResponseEntity<String> response;
 
     @Given("^the application in an integration environment$")
     public void prepareIntegrationEnvironment() throws Throwable {
@@ -20,10 +27,17 @@ public class RequestStepDefs {
 
     @When("^a \"([^\"]*)\" request is made to endpoint \"([^\"]*)\"$")
     public void makeRequest(String requestType, String endpoint) throws Throwable {
+        if (requestType.equals("GET")) {
+            response = template.getForEntity("endpoint", String.class);
+        }
     }
 
     @Then("^the response should have status code (\\d+)$")
-    public void thenStatement(int statusCode) throws Throwable {
+    public void assertResponseStatusCode(int statusCode) throws Throwable {
+        HttpStatus currentStatusCode = response.getStatusCode();
+
+        assertThat("Response status code is not as expected : " +
+                response.getBody(), currentStatusCode.value(), is(statusCode));
     }
 
 }
