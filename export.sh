248a0593a60736402f6e1ee15eabc18d7f398af1
Randy Gingeleski <rjg26247@gmail.com>
2020-04-11 12:59:00 -0400

    close #32 - developed vm

---
+++ export.sh
@@ -0,0 +1,1 @@
+export PATH=$PATH:/opt/gradle/gradle-6.3/bin
--- setup.sh
+++ setup.sh
@@ -1,5 +1,24 @@
-# VAGRANT SETUP SCRIPT FOR FRESH UBUNTU IMAGE
+echo "[SETUP] VAGRANT SETUP SCRIPT FOR FRESH UBUNTU IMAGE"
 
-# Make sure existing packages are up-to-date
+echo "[SETUP] Make sure existing packages are up-to-date"
 sudo apt-get --assume-yes update
 sudo apt-get --assume-yes upgrade
+
+echo "[SETUP] Install JDK (Java 13)"
+sudo apt-get --assume-yes install openjdk-13-jdk
+
+echo "[SETUP] Make sure we're in personal folder, then download Gradle 6.3"
+cd ~
+curl -LO 'https://services.gradle.org/distributions/gradle-6.3-bin.zip'
+
+echo "[SETUP] Install unzip then unzip Gradle 6.3"
+sudo apt-get --assume-yes install unzip
+sudo mkdir /opt/gradle
+sudo unzip -d /opt/gradle gradle-6.3-bin.zip
+
+echo "[SETUP] Add Gradle 6.3 to PATH"
+sudo echo "source /vagrant/scripts/export.sh" >> /home/vagrant/.bashrc
+
+echo "[SETUP] Do an initial compile of the project"
+cd /vagrant
+/opt/gradle/gradle-6.3/bin/gradle assemble
